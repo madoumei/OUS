9c9f27843e95ac27a63e29ab11ce0008303b49d6
jiacun <ryan_uuu@163.com>
2022-10-27 18:44:36 +0800

    3

---
+++ src/main/java/com/client/controller/QcvRoadController.java
@@ -0,0 +1,10 @@
+package com.client.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+
+public class QcvRoadController {
+}
